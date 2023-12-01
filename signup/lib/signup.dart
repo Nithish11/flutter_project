268641bfd5d30c4476495c198208db5fa71d6106
Nithish11 <nithishusha31@gmail.com>
2023-12-01 15:48:49 +0530

    this is updated code

--- signup/lib/signup.dart
+++ signup/lib/signup.dart
@@ -6,41 +6,20 @@ class signup extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color.fromARGB(255, 130, 158, 182),
-      body: Center(
-        child: Container(
-          height: 400,
-          width:400,
-          decoration: BoxDecoration(
-            color: Color.fromARGB(255, 101, 134, 196),
-            borderRadius: BorderRadius.circular(15)
-          ),
-          child: Padding(
-            padding: const EdgeInsets.all(25.0),
-            child: Column(
-              children: [
-                Center(
-                  child: Text(
-                    "Sign Up",
-                    style: TextStyle(
-                      fontSize: 30,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white
-                    ),
+      backgroundColor: Colors.blue,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+                  height: 170,
+                  width: 200,
+                  decoration: BoxDecoration(
+                color: Colors.red,
                   ),
+                  child: Text("Hii santhosh"),
                 ),
-                Row(
-                  children: [
-                    TextField(
-                      decoration: InputDecoration()
-                    )
-                  ],
-                )
-              ],
-            ),
-          ),
-        ),
-      ),
+        ],
+      )
     );
   }
 }
---
+++ signup/lib/structure.py
@@ -0,0 +1,6 @@
+import pandas as pd
+report=pd.read_csv("food price.csv")
+report
+report=pd.read_csv("foodprice.csv")
+print(report.head())
+print(report.dtype)
